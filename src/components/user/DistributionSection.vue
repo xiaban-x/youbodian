3947aa2935ad65ba8f033ae93046aa150ad3da40
xiaban <980892894@qq.com>
2023-09-22 21:43:27 +0800

    我的分销ui完成

---
+++ src/components/user/DistributionSection.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="DSContainer">
+    <div class="top">
+      <div class="title">
+        <!-- 标题插槽 -->
+        <slot name="title"></slot>
+      </div>
+      <div class="source">
+        <!-- 来源插槽 -->
+        <slot name="source"></slot>
+      </div>
+    </div>
+
+    <div class="bottom">
+      <div class="time">
+        <!-- 完成时间插槽 -->
+        <slot name="time"></slot>
+      </div>
+      <div class="money">
+        <!-- 金额插槽 -->
+        + <slot name="money"></slot>
+      </div>
+    </div>
+  </div>
+  <div class="longLine"></div>
+</template>
+
+<style scoped>
+.DSContainer {
+  width: 347px;
+  padding: 10px 14px 14px 13px;
+  border-radius: 5px;
+  background-color: #fff;
+
+  .top {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    .title {
+      color: #292929;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+    .source {
+      color: #222;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+  }
+  .bottom {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    color: #999;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 300;
+    margin-top: 7px;
+    line-height: normal;
+    .money {
+      color: #292929;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+  }
+}
+.longLine {
+  width: 316px;
+  border: 1px #e7e7e7 solid;
+  margin-left: 17px;
+}
+</style>
+<script setup></script>
--- src/views/user/MyDistribution.vue
+++ src/views/user/MyDistribution.vue
@@ -1,13 +1,80 @@
 <script setup>
-
+import Head from '@/components/Head.vue'
+import DistributionSection from '@/components/user/DistributionSection.vue'
 </script>
 
 <template>
-<div>
   <Head>我的分销</Head>
-</div>
+  <div class="MDContainer">
+    <div class="data">
+      <div class="dataCon">
+        <div class="text">注册人数</div>
+        <div class="count">45</div>
+      </div>
+      <div class="dataCon">
+        <div class="text">购买人数</div>
+        <div class="count">210</div>
+      </div>
+      <div class="dataCon">
+        <div class="text">累积收益</div>
+        <div class="count">58.42</div>
+      </div>
+    </div>
+    <div class="distributionDetail">
+      <DistributionSection>
+        <template #title>用户老王</template>
+        <template #source>注册</template>
+        <template #time>2022.05.12 12:16</template>
+        <template #money>250.00</template>
+      </DistributionSection>
+      <DistributionSection>
+        <template #title>用户老王</template>
+        <template #source>注册</template>
+        <template #time>2022.05.12 12:16</template>
+        <template #money>250.00</template>
+      </DistributionSection>
+      <DistributionSection>
+        <template #title>用户老王</template>
+        <template #source>注册</template>
+        <template #time>2022.05.12 12:16</template>
+        <template #money>250.00</template>
+      </DistributionSection>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.MDContainer {
+  padding-bottom: 50px;
+  height: 600px;
+  margin-left: 14px;
+  margin-right: 14px;
+  margin-top: 17px;
+  .data {
+    display: flex;
+    justify-content: space-between;
+    background-color: #fff;
+    border-radius: 5px;
+    margin-bottom: 9px;
+    padding: 16px 30px;
+    .dataCon {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      .count {
+        color: #292929;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+      }
+    }
+  }
+  .distributionDetail {
+    background-color: #fff;
+    border-radius: 5px;
+    padding-bottom: 41px;
+  }
+}
 </style>
